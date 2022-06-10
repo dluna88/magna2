b606b0739dad799076afec8314f9833b7db01c65
Diego Luna Cazares <dluna88@gmail.com>
2022-06-09 19:04:39 -0500

    Grid añadido y menus de navegación

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,89 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="row">
+        <div class="col-9">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+            <div class="container">
+                <div id="toolbars" class="row mb-3">
+                    <div id="toolbar1" class="btn-group d-none" role="group">
+                        <button type="button" class="btn btn-outline-primary">Left</button>
+                        <button type="button" class="btn btn-outline-primary">Middle</button>
+                        <button type="button" class="btn btn-outline-primary">Right</button>
+                        <button type="button" class="btn btn-outline-primary">Right</button>
+                        <button type="button" class="btn btn-outline-primary">Right</button>
+                        <button type="button" class="btn btn-outline-primary">Right</button>
+                    </div>
                 </div>
             </div>
+
+            <div id="maingrid" class="container d-none">
+                <div id="a" class="row g-0">
+                    <div id="a1" class="col square"></div>
+                    <div id="a2" class="col square"></div>
+                    <div id="a3" class="col square"></div>
+                    <div id="a4" class="col square"></div>
+                </div>
+                <div id="b" class="row g-0">
+                    <div id="b1" class="col square"></div>
+                    <div id="b2" class="col square"></div>
+                    <div id="b3" class="col square"></div>
+                    <div id="b4" class="col square"></div>
+                </div>
+                <div id="c" class="row g-0">
+                    <div id="c1" class="col square"></div>
+                    <div id="c2" class="col square"></div>
+                    <div id="c3" class="col square"></div>
+                    <div id="c4" class="col square"></div>
+                </div>
+                <div id="d" class="row g-0">
+                    <div id="d1" class="col square"></div>
+                    <div id="d2" class="col square"></div>
+                    <div id="d3" class="col square"></div>
+                    <div id="d4" class="col square"></div>
+                </div>
+                
+            </div>
+
+        </div>
+        <div class="col-3 d-flex align-items-start">
+            
+            <!-- SIDEBAR -->
+            <ul id="sidebar" class="list-group">
+                <li id="sidebarButton1" class="list-group-item sidebarButton active">An item</li>
+                <li id="sidebarButton2" class="list-group-item sidebarButton">A second item</li>
+                <li id="sidebarButton3" class="list-group-item sidebarButton">A third item</li>
+                <li id="sidebarButton4" class="list-group-item sidebarButton">A fourth item</li>
+                <li id="sidebarButton5" class="list-group-item sidebarButton">And a fifth one</li>
+            </ul>
+            <!-- end SIDEBAR -->
+
         </div>
     </div>
 </div>
+
+@endsection
+
+@section('js')
+<script type="text/javascript">
+
+    $(document).ready(function(){
+        
+        $("#sidebarButton1").click(() => {
+            hidegrid()
+        })
+
+        $("#sidebarButton2").click(function(){
+            showgrid()
+        })
+
+        $(".sidebarButton").click(function(){ $(".sidebarButton").removeClass("active"); $(this).addClass("active"); })
+        
+        const showgrid = () => { $("#maingrid").removeClass("d-none"); $("#maingrid").addClass("d-block") }
+        const hidegrid = () => { $("#maingrid").removeClass("d-block"); $("#maingrid").addClass("d-none") }
+    })
+
+</script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm bbred3">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -79,5 +79,9 @@
             @yield('content')
         </main>
     </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    @yield('js')
 </body>
 </html>
