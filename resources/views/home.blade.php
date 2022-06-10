99003a6b275fde7036238e12c0c146ae6b1ad1a6
Diego Luna Cazares <dluna88@gmail.com>
2022-06-10 00:48:21 -0500

    Sistema de navegación simplificado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,78 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-9">
+        <div class="col-8 g-0">
 
             <div class="container">
-                <div id="toolbars" class="row mb-3">
-                    <div id="toolbar1" class="btn-group d-none" role="group">
-                        <button type="button" class="btn btn-outline-primary">Left</button>
-                        <button type="button" class="btn btn-outline-primary">Middle</button>
-                        <button type="button" class="btn btn-outline-primary">Right</button>
-                        <button type="button" class="btn btn-outline-primary">Right</button>
-                        <button type="button" class="btn btn-outline-primary">Right</button>
-                        <button type="button" class="btn btn-outline-primary">Right</button>
+                <div class="row mb-3">
+                    <div id="tb-uploadfile"  class="row mb-3 justify-content-center toolbar">
+                        <div class="col-10">
+                            <div class="card">
+                                <div class="card-body">
+                                    <form method="post" action="">
+                                        @csrf
+                                        <label for="formFile" class="form-label">
+                                            <h4><i class="fa-solid fa-paperclip"></i> Attach file (optional)</h4>
+                                            <p>If you have a previous model or want to enrich with more information, you can upload a file here.</p>
+                                        </label>
+                                        <input class="form-control" type="file" id="formFile">
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="tb-geometry" class="btn-group d-none toolbar" role="group">
+                        <button type="button" class="btn btn-secondary">Geometry</button>
+                        <button type="button" class="btn btn-secondary">Middle</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                    </div>
+                    <div id="tb-shield" class="btn-group d-none toolbar" role="group">
+                        <button type="button" class="btn btn-secondary">Shield</button>
+                        <button type="button" class="btn btn-secondary">Middle</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                    </div>
+                    <div id="tb-spotting" class="btn-group d-none toolbar" role="group">
+                        <button type="button" class="btn btn-secondary">Spotting</button>
+                        <button type="button" class="btn btn-secondary">Middle</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                    </div>
+                    <div id="tb-robots" class="btn-group d-none toolbar" role="group">
+                        <button type="button" class="btn btn-secondary">Robots</button>
+                        <button type="button" class="btn btn-secondary">Middle</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                    </div>
+                    <div id="tb-buffer" class="btn-group d-none toolbar" role="group">
+                        <button type="button" class="btn btn-secondary">Buffer</button>
+                        <button type="button" class="btn btn-secondary">Middle</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                    </div>
+                    <div id="tb-conveyors" class="btn-group d-none toolbar" role="group">
+                        <button type="button" class="btn btn-secondary">Conveyors</button>
+                        <button type="button" class="btn btn-secondary">Middle</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                    </div>
+                    <div id="tb-fencing" class="btn-group d-none toolbar" role="group">
+                        <button type="button" class="btn btn-secondary">fencing</button>
+                        <button type="button" class="btn btn-secondary">Middle</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
+                        <button type="button" class="btn btn-secondary">Right</button>
                     </div>
                 </div>
             </div>
@@ -48,15 +109,35 @@
             </div>
 
         </div>
-        <div class="col-3 d-flex align-items-start">
+        <div class="col-4 d-flex align-items-start">
             
             <!-- SIDEBAR -->
             <ul id="sidebar" class="list-group">
-                <li id="sidebarButton1" class="list-group-item sidebarButton active">An item</li>
-                <li id="sidebarButton2" class="list-group-item sidebarButton">A second item</li>
-                <li id="sidebarButton3" class="list-group-item sidebarButton">A third item</li>
-                <li id="sidebarButton4" class="list-group-item sidebarButton">A fourth item</li>
-                <li id="sidebarButton5" class="list-group-item sidebarButton">And a fifth one</li>
+                <li id="uploadfile" class="list-group-item sidebarButton active">
+                    <i class="fa-regular fa-file-lines"></i> Previous Project
+                </li>
+                <li id="geometry" class="list-group-item sidebarButton">
+                    <i class="fa-solid fa-bezier-curve"></i> Geometry stations
+                </li>
+                <li id="shield" class="list-group-item sidebarButton">
+                    <i class="fa-solid fa-shield-halved"></i> Shield stations
+                </li>
+                <li id="spotting" class="list-group-item sidebarButton">
+                    <i class="fa-brands fa-hubspot"></i> Spotting stations
+                </li>
+                <li id="robots" class="list-group-item sidebarButton">
+                    <i class="fa-solid fa-robot"></i> Robots
+                </li>
+                <li id="buffer" class="list-group-item sidebarButton">
+                    <i class="fa-brands fa-buffer"></i> Buffer
+                </li>
+                <li id="conveyors" class="list-group-item sidebarButton">
+                    <i class="fa-solid fa-bandage"></i> Conveyors
+                </li>
+                <li id="fencing" class="list-group-item sidebarButton">
+                    <i class="fa-solid fa-border-none"></i> Fencing
+                </li>
+
             </ul>
             <!-- end SIDEBAR -->
 
@@ -67,22 +148,21 @@
 @endsection
 
 @section('js')
+<script src="https://kit.fontawesome.com/d1bdffa7aa.js" crossorigin="anonymous"></script>
 <script type="text/javascript">
 
     $(document).ready(function(){
         
-        $("#sidebarButton1").click(() => {
-            hidegrid()
-        })
-
-        $("#sidebarButton2").click(function(){
-            showgrid()
-        })
-
-        $(".sidebarButton").click(function(){ $(".sidebarButton").removeClass("active"); $(this).addClass("active"); })
         
-        const showgrid = () => { $("#maingrid").removeClass("d-none"); $("#maingrid").addClass("d-block") }
-        const hidegrid = () => { $("#maingrid").removeClass("d-block"); $("#maingrid").addClass("d-none") }
+
+        $(".sidebarButton").click(function(){
+            $(".sidebarButton").removeClass('active')
+            $(this).addClass('active')
+            let id = $(this).attr('id')
+            $(".toolbar").addClass('d-none')
+            $("#tb-"+id).removeClass('d-none')
+            if(id != "uploadfile"){ $("#maingrid").removeClass('d-none') }else{ $("#maingrid").addClass('d-none') }
+        })
     })
 
 </script>
