5277ba67a7ca39c911b24c8e6bc04381df3fc2d1
Diego Luna <dluna88@gmail.com>
2022-06-14 16:03:21 -0500

    Dashboard updates

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,18 +3,61 @@
 @section('content')
 
 <div class="container">
-    <div class="card text-center">
-    <div class="card-header">
-        Featured
+    <div class="row">
+        <div class="col">
+            <div class="card text-center">
+                <div class="card-body">
+                    <h5 class="card-title">Setup a custom layout</h5>
+                    <p class="card-text">Drag and drop tool to easy setp a custom layout of equipment.</p>
+                </div>
+                <div class="card-footer text-muted">
+                    <a href="{{ url('/layout') }}" class="btn btn-primary">Open tool <i class="fa-solid fa-angles-right"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card text-center">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                <div class="card-footer text-muted">
+                    2 days ago
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card text-center">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                <div class="card-footer text-muted">
+                    2 days ago
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-    <div class="card-footer text-muted">
-        2 days ago
-    </div>
+    <div class="row mt-5">
+        <div class="col-8">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit ea suscipit placeat dolore totam minima reiciendis, veritatis asperiores laborum doloremque voluptatum quia cumque possimus obcaecati rem molestias hic soluta eaque.
+        </div>
+        <div style="border-left:solid 1px #ccc;" class="col-4">
+            <div class="card-body">
+                <div class="d-flex flex-column align-items-center text-center">
+                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
+                    <div class="mt-3">
+                        <h4>John Doe</h4>
+                        <p class="text-secondary mb-1">Full Stack Developer</p>
+                        <p class="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
+                        <button class="btn btn-primary">Follow</button>
+                        <button class="btn btn-outline-primary">Message</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
