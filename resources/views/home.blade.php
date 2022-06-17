de76254cdbfaeca3b2a1427a12a5cad035074b00
GenisSenpapi <jorgi.rojo.chivas@gmail.com>
2022-06-17 16:05:40 -0500

    Actualizaciones

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -56,24 +56,120 @@
                     <div id="a2" class="col square"></div>
                     <div id="a3" class="col square"></div>
                     <div id="a4" class="col square"></div>
+                    <div id="a5" class="col square"></div>
+                    <div id="a6" class="col square"></div>
+                    <div id="a7" class="col square"></div>
+                    <div id="a8" class="col square"></div>
+                    <div id="a9" class="col square"></div>
+                    <div id="a10" class="col square"></div>
                 </div>
                 <div id="b" class="row g-0">
                     <div id="b1" class="col square"></div>
                     <div id="b2" class="col square"></div>
                     <div id="b3" class="col square"></div>
                     <div id="b4" class="col square"></div>
+                    <div id="b5" class="col square"></div>
+                    <div id="b6" class="col square"></div>
+                    <div id="b7" class="col square"></div>
+                    <div id="b8" class="col square"></div>
+                    <div id="b9" class="col square"></div>
+                    <div id="b10" class="col square"></div>
                 </div>
                 <div id="c" class="row g-0">
                     <div id="c1" class="col square"></div>
                     <div id="c2" class="col square"></div>
                     <div id="c3" class="col square"></div>
                     <div id="c4" class="col square"></div>
+                    <div id="c5" class="col square"></div>
+                    <div id="c6" class="col square"></div>
+                    <div id="c7" class="col square"></div>
+                    <div id="c8" class="col square"></div>
+                    <div id="c9" class="col square"></div>
+                    <div id="c10" class="col square"></div>
                 </div>
                 <div id="d" class="row g-0">
                     <div id="d1" class="col square"></div>
                     <div id="d2" class="col square"></div>
                     <div id="d3" class="col square"></div>
                     <div id="d4" class="col square"></div>
+                    <div id="d5" class="col square"></div>
+                    <div id="d6" class="col square"></div>
+                    <div id="d7" class="col square"></div>
+                    <div id="d8" class="col square"></div>
+                    <div id="d9" class="col square"></div>
+                    <div id="d10" class="col square"></div>
+                </div>
+                <div id="e" class="row g-0">
+                    <div id="e1" class="col square"></div>
+                    <div id="e2" class="col square"></div>
+                    <div id="e3" class="col square"></div>
+                    <div id="e4" class="col square"></div>
+                    <div id="e5" class="col square"></div>
+                    <div id="e6" class="col square"></div>
+                    <div id="e7" class="col square"></div>
+                    <div id="e8" class="col square"></div>
+                    <div id="e9" class="col square"></div>
+                    <div id="e10" class="col square"></div>
+                </div>
+                <div id="f" class="row g-0">
+                    <div id="f1" class="col square"></div>
+                    <div id="f2" class="col square"></div>
+                    <div id="f3" class="col square"></div>
+                    <div id="f4" class="col square"></div>
+                    <div id="f5" class="col square"></div>
+                    <div id="f6" class="col square"></div>
+                    <div id="f7" class="col square"></div>
+                    <div id="f8" class="col square"></div>
+                    <div id="f9" class="col square"></div>
+                    <div id="f10" class="col square"></div>
+                </div>
+                <div id="g" class="row g-0">
+                    <div id="g1" class="col square"></div>
+                    <div id="g2" class="col square"></div>
+                    <div id="g3" class="col square"></div>
+                    <div id="g4" class="col square"></div>
+                    <div id="g5" class="col square"></div>
+                    <div id="g6" class="col square"></div>
+                    <div id="g7" class="col square"></div>
+                    <div id="g8" class="col square"></div>
+                    <div id="g9" class="col square"></div>
+                    <div id="g10" class="col square"></div>
+                </div>
+                <div id="h" class="row g-0">
+                    <div id="h1" class="col square"></div>
+                    <div id="h2" class="col square"></div>
+                    <div id="h3" class="col square"></div>
+                    <div id="h4" class="col square"></div>
+                    <div id="h5" class="col square"></div>
+                    <div id="h6" class="col square"></div>
+                    <div id="h7" class="col square"></div>
+                    <div id="h8" class="col square"></div>
+                    <div id="h9" class="col square"></div>
+                    <div id="h10" class="col square"></div>
+                </div>
+                <div id="i" class="row g-0">
+                    <div id="i1" class="col square"></div>
+                    <div id="i2" class="col square"></div>
+                    <div id="i3" class="col square"></div>
+                    <div id="i4" class="col square"></div>
+                    <div id="i5" class="col square"></div>
+                    <div id="i6" class="col square"></div>
+                    <div id="i7" class="col square"></div>
+                    <div id="i8" class="col square"></div>
+                    <div id="i9" class="col square"></div>
+                    <div id="i10" class="col square"></div>
+                </div>
+                <div id="j" class="row g-0">
+                    <div id="j1" class="col square"></div>
+                    <div id="j2" class="col square"></div>
+                    <div id="j3" class="col square"></div>
+                    <div id="j4" class="col square"></div>
+                    <div id="j5" class="col square"></div>
+                    <div id="j6" class="col square"></div>
+                    <div id="j7" class="col square"></div>
+                    <div id="j8" class="col square"></div>
+                    <div id="j9" class="col square"></div>
+                    <div id="j10" class="col square"></div>
                 </div>
             </div>
         </div>
